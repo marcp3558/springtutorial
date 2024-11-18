ff91c2230b2f44b613e65476300d487c8e09502c
marc p <marcp3558@gmail.com>
2024-11-17 19:56:32 -0500

    add demo

---
+++ demo2/src/main/java/com/example/demo/DemoApplication.java
@@ -0,0 +1,32 @@
+package com.example.demo;
+import java.util.Map;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class DemoApplication {
+    public static void main(String[] args) {
+      SpringApplication.run(DemoApplication.class, args);
+    }
+
+    @GetMapping("/hello")
+    public String hello(@RequestParam(value = "name", defaultValue = "World") String name) {
+      return String.format("Hello %s!", name);
+    }
+
+	@PostMapping(value = "testPost")
+	public ResponseEntity testPost(@RequestBody Map<String, Object> requestObject) {
+		return new ResponseEntity<>(
+			"you sent " + requestObject.toString(),
+			HttpStatus.OK);
+	}
+}
