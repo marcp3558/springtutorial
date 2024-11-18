55b680ba897a426b07907e0481fb442de9ad5662
marcp3558 <31511573+marcp3558@users.noreply.github.com>
2024-11-17 19:58:29 -0500

    Update DemoApplication.java tabs

--- demo2/src/main/java/com/example/demo/DemoApplication.java
+++ demo2/src/main/java/com/example/demo/DemoApplication.java
@@ -13,18 +13,22 @@ import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @RestController
-public class DemoApplication {
-    public static void main(String[] args) {
-      SpringApplication.run(DemoApplication.class, args);
-    }
+public class DemoApplication 
+{
+	public static void main(String[] args) 
+	{
+		SpringApplication.run(DemoApplication.class, args);
+	}
 
-    @GetMapping("/hello")
-    public String hello(@RequestParam(value = "name", defaultValue = "World") String name) {
-      return String.format("Hello %s!", name);
-    }
+	@GetMapping("/hello")
+	public String hello(@RequestParam(value = "name", defaultValue = "World") String name) 
+	{
+	      return String.format("Hello %s!", name);
+    	}
 
 	@PostMapping(value = "testPost")
-	public ResponseEntity testPost(@RequestBody Map<String, Object> requestObject) {
+	public ResponseEntity testPost(@RequestBody Map<String, Object> requestObject) 
+	{
 		return new ResponseEntity<>(
 			"you sent " + requestObject.toString(),
 			HttpStatus.OK);
